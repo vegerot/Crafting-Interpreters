30c1753e468f92878ec09097c323186d0323b04c
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-01-01 13:01:44 -0800

    feat(scanner): start scanner

---
+++ c/scanner.c
@@ -0,0 +1,42 @@
+#include "lox_assert.h"
+#include "scanner.h"
+#include <stdbool.h>
+
+void initScanner(Scanner* scanner, char const* source) {
+	scanner->start_of_lexeme = scanner->current = source;
+	scanner->line = 1;
+}
+
+bool isAtEnd(Scanner* scanner) {
+	return (*scanner->current == '\0');
+}
+
+Token makeToken(Scanner* scanner, TokenType type) {
+	return (Token) {
+		.type = type,
+		.start = scanner->start_of_lexeme,
+		.length = (int)(scanner->current - scanner->start_of_lexeme),
+		.line = scanner->line,
+	};
+}
+
+Token errorToken(Scanner* scanner, char const* message) {
+	return (Token){
+		.type=TOKEN_ERROR,
+		.start = scanner->start_of_lexeme,
+		.line = scanner->line,
+		.error_message = message,
+	};
+}
+
+Token scanToken(Scanner* scanner) {
+	scanner->start_of_lexeme = scanner->current;
+	if (isAtEnd(scanner)) {
+		return makeToken(scanner, TOKEN_EOF);
+	}
+	return errorToken(scanner, "Unexpected character");
+}
+
+char* tokenTypeToString_(TokenType type) {
+	LOX_ASSERT(0 && type && "unimplemented");
+}
