15004e9a3775b64c14f94a0f1e6f61059b5d6ca7
Max 👨🏽‍💻 Coplan <Max.Coplan@walmart.com>
2024-01-01 13:01:44 -0800

    feat(scanner): start scanning tokens

--- c/scanner.c
+++ c/scanner.c
@@ -1,18 +1,18 @@
+#include <stdbool.h>
+#include <string.h>
+
 #include "lox_assert.h"
 #include "scanner.h"
-#include <stdbool.h>
 
 void initScanner(Scanner* scanner, char const* source) {
 	scanner->start_of_lexeme = scanner->current = source;
 	scanner->line = 1;
 }
 
-bool isAtEnd(Scanner* scanner) {
-	return (*scanner->current == '\0');
-}
+bool isAtEnd(Scanner* scanner) { return (*scanner->current == '\0'); }
 
 Token makeToken(Scanner* scanner, TokenType type) {
-	return (Token) {
+	return (Token){
 		.type = type,
 		.start = scanner->start_of_lexeme,
 		.length = (int)(scanner->current - scanner->start_of_lexeme),
@@ -20,11 +20,12 @@ Token makeToken(Scanner* scanner, TokenType type) {
 	};
 }
 
-Token errorToken(Scanner* scanner, char const* message) {
+Token errorToken(Scanner* scanner, char const* message, int length) {
 	return (Token){
-		.type=TOKEN_ERROR,
+		.type = TOKEN_ERROR,
 		.start = scanner->start_of_lexeme,
 		.line = scanner->line,
+		.length = length,
 		.error_message = message,
 	};
 }
@@ -34,7 +35,8 @@ Token scanToken(Scanner* scanner) {
 	if (isAtEnd(scanner)) {
 		return makeToken(scanner, TOKEN_EOF);
 	}
-	return errorToken(scanner, "Unexpected character");
+	return errorToken(scanner, "Unexpected character",
+					  strlen("Unexpected character"));
 }
 
 char* tokenTypeToString_(TokenType type) {
