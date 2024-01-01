c1a030adeea50c08856714956f97ccb96c2c44b4
Max 👨🏽‍💻 Coplan <Max.Coplan@walmart.com>
2024-01-01 13:01:44 -0800

    feat(scanner): skip whitespace

--- c/test/test-scanner.c
+++ c/test/test-scanner.c
@@ -38,7 +38,34 @@ static void single_character_tokens() {
 	LOX_ASSERT_EQUALS(got, want)
 }
 
+static void skip_whitespace() {
+	Scanner scanner;
+	char const* src = "(	{\r,  +\n; ";
+	initScanner(&scanner, src);
+
+	TokenType wants[] = {
+		TOKEN_LEFT_PAREN, TOKEN_LEFT_BRACE, TOKEN_COMMA,
+		TOKEN_PLUS,		  TOKEN_SEMICOLON,
+	};
+
+	for (unsigned long i = 0; i < sizeof(wants) / sizeof(wants[0]); ++i) {
+		Token t = scanToken(&scanner);
+		TokenType want = wants[i];
+		TokenType got = t.type;
+		if (got != want) {
+			fflush(stdout);
+			fprintf(
+				stderr, "wrong token type.  Expected '%c' to be %s, got %s\n",
+				*t.start, tokenTypeToString_(want), tokenTypeToString_(got));
+			LOX_ASSERT(1 == 0);
+		}
+	}
+	/* LOX_ASSERT_EQUALS(scanToken(&scanner).type, TOKEN_EOF); */
+	/* LOX_ASSERT_EQUALS(scanner.line, 2); */
+}
+
 int main(void) {
 	eof();
 	single_character_tokens();
+	skip_whitespace();
 }
