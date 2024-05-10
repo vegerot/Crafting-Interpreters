42935454a72c29def34ed918fedc40f2d2d90aa8
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-05-10 13:17:33 -0700

    wip

--- c/src/value.h
+++ c/src/value.h
@@ -1,8 +1,48 @@
 #ifndef clox_value_h
 #define clox_value_h
 #include "common.h"
+#include <stdint.h>
+#include <assert.h>
 
-typedef double Value;
+typedef enum { 
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER
+} ValueType;
+
+typedef struct {
+	union {
+		bool boolean;
+		double number;
+	} as;
+	ValueType type;
+} Value;
+
+typedef struct {
+	struct {
+		union {
+			bool boolean;
+			double number;
+		} as;
+		ValueType type;
+		ValueType type2;
+	};
+} sixteenByteValue;
+
+static_assert(sizeof(sixteenByteValue) == 16);
+
+typedef struct {
+	struct {
+		union {
+			bool boolean;
+			double number;
+		} as;
+		ValueType type;
+	};
+	ValueType type2;
+} twentyFourByteValue;
+
+static_assert(sizeof(twentyFourByteValue) == 24);
 
 typedef struct {
 	int capacity;
