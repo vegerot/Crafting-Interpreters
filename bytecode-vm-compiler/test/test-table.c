a4c26f9fe7400c9db0da39195bf9e39f82cfc032
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-11-01 17:29:07 -0700

    @@@ wip: hashtable

--- bytecode-vm-compiler/test/test-table.c
+++ bytecode-vm-compiler/test/test-table.c
@@ -1,6 +1,41 @@
+#include "../src/lox_assert.h"
 #include "../src/vm.h"
 #include <string.h>
-LoxString* allocateString(VM* vm, char const* cString, int length);
+LoxString* allocateString(VM* vm, char const* cString, size_t length);
+
+#include "../src/common.h"
+#include "../src/memory.h"
+#include "../src/value.h"
+
+typedef struct {
+	LoxString* key;
+	Value value;
+} TableEntry;
+
+typedef struct {
+	size_t count;
+	size_t capacity;
+	TableEntry* entries;
+} LoxTable;
+
+void tableInit(LoxTable* table);
+void tableInit(LoxTable* table) {
+	table->count = 0;
+	table->capacity = table->count;
+	table->entries = NULL;
+}
+void tableFree(LoxTable* table);
+void tableFree(LoxTable* table) {
+	if (table->entries == NULL) {
+		LOX_ASSERT(false && "trying to free uninitialized table");
+	}
+	FREE_ARRAY(Entry, table->entries, table->capacity);
+}
+bool tableSet(LoxTable* table, LoxString* key, Value value);
+bool tableSet(LoxTable* table, LoxString* key, Value value) {}
+Value* tableGet(LoxTable* table, LoxString* key);
+Value* tableGet(LoxTable* table, LoxString* key) {}
+
 int main(void) {
 	// basic
 	initVM();
@@ -9,6 +44,11 @@ int main(void) {
 	tableInit(&table);
 	char const* k = "key";
 	LoxString* key = allocateString(&vm, "key", strlen(k));
-	tableSet(&table, key, NUMBER_VAL(42.0));
+	Value want = NUMBER_VAL(42.0);
+	tableSet(&table, key, want);
 
+	Value* res = tableGet(&table, key);
+
+	LOX_ASSERT_VALUE_EQUALS(*res, want);
+	tableFree(&table);
 }
